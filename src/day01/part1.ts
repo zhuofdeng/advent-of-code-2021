01d27ad220f40e019c9ee14592c1ae05c441d1c9
Zhuo Deng <zhuofdeng@gmail.com>
2021-12-01 20:31:52 -0500

    day1 puzzle answers

--- src/day01/part1.ts
+++ src/day01/part1.ts
@@ -1,5 +1,15 @@
 // Advent of Code - Day 1 - Part One
 
 export function part1(input: string): number {
-  return 0;
+  const depths = input.split('\n');
+  let count = 0;
+  let previousDepth = 0;
+  depths.forEach((depth) => {
+    const currentDepth = parseInt(depth);
+    if (currentDepth > previousDepth && previousDepth > 0) {
+      count ++;
+    }
+    previousDepth = parseInt(depth)
+  });
+  return count;
 }
--- src/day01/part2.ts
+++ src/day01/part2.ts
@@ -1,5 +1,23 @@
 // Advent of Code - Day 1 - Part Two
 
+const sumThreeDepths = (depths: Array<number>, index: number): number => {
+  let sum = 0;
+  for(let i = index; i < index + 3; i++) {
+    sum += depths[i];
+  }
+  return sum;
+}
+
 export function part2(input: string): number {
-  return 0;
+  const depths = input.split('\n').map((value) => parseInt(value));
+  let count = 0;
+  let previousSum = sumThreeDepths(depths, 0);
+  for(let i = 1; i < depths.length - 2; i++) {
+    const currentSum = sumThreeDepths(depths, i);
+    if (currentSum > previousSum) {
+      count++
+    }
+    previousSum = currentSum;
+  }
+  return count;
 }
